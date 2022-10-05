09eb703a4740c76ec1e889ad7d06ca81f17dd613
MarilynWho <painsides@gmail.com>
2022-10-05 17:57:00 +0100

    Drawing on canvas

--- src/index.js
+++ src/index.js
@@ -3,9 +3,31 @@ window.onload = function () {
     hit.onclick = hitfunc;
     let canvas = document.getElementById("canvas");
     let context = canvas.getContext("2d");
+    
+    context.beginPath();
     context.moveTo(50, 100);
     context.lineTo(400, 200);
-    context.stroke();
+
+    context.beginPath();
+    context.rect(50, 50, 200, 150);
+    
+    context.fillStyle = "#5e9860";
+    context.fill();
+    
+    context.beginPath();
+    context.arc(150, 150, 100, 0, 2 * Math.PI);
+    
+    context.fillStyle = "#5e9860";
+    context.fill();
+
+    context.lineWIdth = 10;
+    context.strokeStyle = "af1f1f";
+
+    // context.stroke();
+    context.font = "42px Arial";
+
+    context.fillText("Hello World", 300, 100);
+    
 };
 
 let health = 100;
